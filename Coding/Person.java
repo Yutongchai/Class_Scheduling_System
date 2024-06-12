75fc3a1b6eadc398760cb073852309037ea27e57
MUHAMMAD NUR AZHAR  BIN MOHD YAZID <muhammadnurazhar@graduate.utm.my>
2024-06-12 14:48:03 +0800

    Update Person.java

--- Coding/Person.java
+++ Coding/Person.java
@@ -6,6 +6,8 @@ enum Day {
 // Abstract Class for Person
 public abstract class Person {
     private String name;
+    private String username;
+    private String password;
     private String email;
     private String phoneNumber;
 
@@ -14,12 +16,29 @@ public abstract class Person {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
+
     public void setName(String n) {
         this.name = n;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setUsername(String u) {
+        this.username = u;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String p) {
+        this.password = p;
+    }
+
+    public String getPassword() {
+        return password;
     }
     
     public void setEmail(String e) {
@@ -39,6 +58,6 @@ public abstract class Person {
     }
 
     public String getDetails() {
-        return "Name: " + name + ", \nEmail Address: " + email + ", \nPhone Number: " + phoneNumber + "\n";
+        return "Name: " + name + ", \nEmail Address: " + email + ", \nPhone Number: " + phoneNumber + "\nUsername: " + username + "\n";
     }
 }

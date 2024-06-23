32b9e8ff6b7d8c09c7030db1e15cb9e864e98574
MUHAMMAD NUR AZHAR  BIN MOHD YAZID <muhammadnurazhar@graduate.utm.my>
2024-06-23 23:30:03 +0800

    log out edit

--- Coding/HomeTuitionSystem.java
+++ Coding/HomeTuitionSystem.java
@@ -61,6 +61,7 @@ public class HomeTuitionSystem {
                         }
                         break;
                     case 4:
+                        System.out.println("Student " + student.getName() + " has logged out. Thank You");
                         return;
                     default:
                         System.out.println("Invalid choice. Please try again.");
@@ -109,7 +110,7 @@ public class HomeTuitionSystem {
                         System.out.print("*********************************************************\n");
                         break;
                     case 3:
-                        System.out.println("Logging out...");
+                    System.out.println("Admin has logged out. Thank You");
                         return;
                     default:
                         System.out.println("Invalid choice. Please try again.");
